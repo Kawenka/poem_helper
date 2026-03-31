4375b1bbdd0730d47ad3f95ec53e94cfb04fd404
kawenka <silvestrekewan@gmail.com>
2026-03-31 15:14:11 +0200

    feat: update main loop with interactive ADD, DISPLAY, and SAVE commands

--- main.cpp
+++ main.cpp
@@ -1,16 +1,54 @@
 #include "inc/SyllabCounter.hpp"
+#include "inc/Color.hpp"
 #include <string>
 
 int	main(void)
 {
 	SyllableCounter	sc;
-    std::string ver;
+    std::string verse;
+    std::string save;
 
     while (1)
     {
-        if (!std::getline(std::cin, ver))
+        std::cout << BOLD_WHITE << "input : " << RESET;
+        if (!std::getline(std::cin, verse))
             break;
-        std::cout << "Syllabes : " <<sc.countVerseSyllables(ver) << std::endl;
+
+        if (!verse.compare("ADD"))
+        {
+            sc.addVerse(save);
+            continue;
+        }
+
+        if (!verse.compare("DISPLAY"))
+        {
+            sc.displayPoem();
+            continue;
+        }
+
+        if (!verse.compare("SAVE"))
+        {
+            try
+            {
+                std::string filename;
+                std::cout << "Filename : ";
+                if (!std::getline(std::cin, filename))
+                {
+                    std::cout << RED << "Failed" << RESET << std::endl;
+                    continue;
+                }
+                sc.savePoem(filename);
+                continue;
+            }
+            catch(const std::exception &e)
+            {
+                std::cerr << e.what() << std::endl;
+                continue;
+            }
+        }
+
+        save = verse;
+        std::cout << YELLOW << "Syllabes : " <<sc.countVerseSyllables(verse) << RESET << std::endl;
     }
 	return (0);
 }
