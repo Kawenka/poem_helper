462c7760e12b9172af6fdbd916b58aac09ffb71d
kawenka <silvestrekewan@gmail.com>
2026-03-31 16:25:53 +0200

    feat: enhance poem display with line numbers, counts and verse types

--- main.cpp
+++ main.cpp
@@ -47,6 +47,9 @@ int	main(void)
             }
         }
 
+        if (!verse.compare("EXIT"))
+            break;
+
         save = verse;
         std::cout << YELLOW << "Syllabes : " <<sc.countVerseSyllables(verse) << RESET << std::endl;
     }
